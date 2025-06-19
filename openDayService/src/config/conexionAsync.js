ff52bf5037e9008e7e48b42548ab80070fd0ea23
Cristian Ruben <cristian.figueroa@inssoftmx.com>
2025-06-19 09:57:04 -0600

    agregar manejo de errores para la conexion a BD

--- openDayService/src/config/conexionAsync.js
+++ openDayService/src/config/conexionAsync.js
@@ -2,13 +2,21 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.on('connection', () => {
+  console.log('Connected to the database');
+});
+
+pool.on('error', (error) => {
+  console.error('Error connecting to the database', error);
 });
 
 module.exports = pool;
