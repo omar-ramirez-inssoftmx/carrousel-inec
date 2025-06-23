8b18c1212734b0e7a05d9cc46d984e701984a681
Cristian Ruben <cristian.figueroa@inssoftmx.com>
2025-06-22 18:28:17 -0600

    Remove unused testing libraries and files

--- openpay-project/src/App.js
+++ openpay-project/src/App.js
@@ -15,31 +15,30 @@ import TerminosCondiciones from './components/TerminosCondiciones';
 import AvisoPrivacidad from './components/AvisoPrivacidad';
 import Contacto from './components/Contacto';
 
-
 const queryClient = new QueryClient();
 
 function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/info/student" element={<InfoStudent />} />
-                    <Route path="/dashboard/pedidos" element={<PedidosTable />} />
-                    <Route path="/dashboard/CheckLinks" element={<CheckLinks />} />
-                    <Route path="/dashboard/CreateCard" element={<CreateCard />} />
-                    <Route path="/dashboard/Activity" element={<Activity />} />
-                    <Route path="/dashboard/ListCard" element={<ListCard />} />
-                    <Route path="/dashboard/DetailCard" element={<DetailCard />} />
-                    <Route path="/dashboard/EditCard" element={<EditCard />} />                    
-                    <Route path="/dashboard/DetailActivity" element={<DetailActivity />} />  
-                    <Route path="/terminos-condiciones" element={<TerminosCondiciones />} />    
-                    <Route path="/aviso-privacidad" element={<AvisoPrivacidad />} />  
-                    <Route path="/contacto" element={<Contacto />} />                    
-                </Routes>
-            </Router>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/info/student" element={<InfoStudent />} />
+          <Route path="/dashboard/pedidos" element={<PedidosTable />} />
+          <Route path="/dashboard/CheckLinks" element={<CheckLinks />} />
+          <Route path="/dashboard/CreateCard" element={<CreateCard />} />
+          <Route path="/dashboard/Activity" element={<Activity />} />
+          <Route path="/dashboard/ListCard" element={<ListCard />} />
+          <Route path="/dashboard/DetailCard" element={<DetailCard />} />
+          <Route path="/dashboard/EditCard" element={<EditCard />} />
+          <Route path="/dashboard/DetailActivity" element={<DetailActivity />} />
+          <Route path="/terminos-condiciones" element={<TerminosCondiciones />} />
+          <Route path="/aviso-privacidad" element={<AvisoPrivacidad />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- openpay-project/src/utils/GeneralMethods.js
+++ openpay-project/src/utils/GeneralMethods.js
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { listMatriculaStudentCard, listMatriculaStudentCardActive, listMatriculaStudentOrders  } from '../api';
 
 //Tarjetas del alumno
